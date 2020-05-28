8fff05175fb0dbf238f02dd745eff8ff3b2bf45b
Jacosn Rodrigues <62717829+jacksrm@users.noreply.github.com>
2020-05-28 09:11:33 -0300

    Updates

---
+++ precache-manifest.e33ea472c0a12c044411503f457c61a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99c4723bf308bc4a524b712b5148be4",
+    "url": "/permutation-generator/index.html"
+  },
+  {
+    "revision": "370c0d6038dd6fcfa4b1",
+    "url": "/permutation-generator/static/css/main.d7e4dc77.chunk.css"
+  },
+  {
+    "revision": "86c6f666a5e901147bff",
+    "url": "/permutation-generator/static/js/2.d8ecbd75.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/permutation-generator/static/js/2.d8ecbd75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "370c0d6038dd6fcfa4b1",
+    "url": "/permutation-generator/static/js/main.d5886ad7.chunk.js"
+  },
+  {
+    "revision": "0362b4c852b281a85a0e",
+    "url": "/permutation-generator/static/js/runtime-main.ffeba7d6.js"
+  },
+  {
+    "revision": "e672988fce7f4ad88dd633f6d8da0a2a",
+    "url": "/permutation-generator/static/media/logo_green.e672988f.svg"
+  }
+]);
